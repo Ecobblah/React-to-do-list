eb73535d4d49beae004d8cd5d675aa5c31252788
Emmanuel Cobblah <Ecobblah@outlook.com>
2019-02-25 17:31:47 -0500

    Added comments and spaces

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import React, { Component } from 'react';
-
 import Logo from './Logo';
 import Items from './Items';
 import AddItem from "./AddItem";
 
 class App extends Component {
+  
   state={
     userList:[],
     numItems: 0
   }
-  handleDelete=(i) => {
+  
+  // Delete
+  handleDelete = (i) => {
+    // removed the selected item i from array
     const userListArray = this.state.userList.filter(x=>{
       return x.id !== i;
     })
-
+    
+    // update state
     this.setState({
       userList: userListArray
     })
   }
-  handleAdd =(i) => {
+  
+  // Add
+  handleAdd = (i) => {
+    // duplicate array and insert i to it
     const userListArray = [...this.state.userList,i]
+    
+    // update state
     this.setState({
       userList: userListArray,
       numItems: this.state.numItems+1
     })
   }
+  
   render() {
     return (
       <div className="App">
